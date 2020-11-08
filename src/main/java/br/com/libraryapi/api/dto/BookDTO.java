63bcbae3363620e3963ac6ca9b65f9d3250f7301
Jean <jxs.jean@gmail.com>
2020-11-08 00:59:48 -0300

    Add BookDTO

---
+++ src/main/java/br/com/libraryapi/api/dto/BookDTO.java
@@ -0,0 +1,15 @@
+package br.com.libraryapi.api.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookDTO {
+    private Long id;
+    private String title;
+    private String author;
+    private String isbn;
+}
