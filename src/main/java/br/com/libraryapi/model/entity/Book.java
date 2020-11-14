43061dce49db0e8c93bea532e67a464df5834083
Jean <jxs.jean@gmail.com>
2020-11-14 19:05:32 -0300

    Mapping Book entity with JPA.

--- src/main/java/br/com/libraryapi/model/entity/Book.java
+++ src/main/java/br/com/libraryapi/model/entity/Book.java
@@ -2,12 +2,18 @@ package br.com.libraryapi.model.entity;
 
 import lombok.*;
 
+import javax.persistence.*;
+
 @Setter
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "book")
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String author;
