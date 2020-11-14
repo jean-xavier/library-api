7af4d22dae4341aaa7ca9514a662d0e9dd59010d
Jean <jxs.jean@gmail.com>
2020-11-13 23:45:58 -0300

    Implements unit test for scene where should not allow saving isbn duplicated.

--- src/test/java/br/com/libraryapi/services/BookServiceTest.java
+++ src/test/java/br/com/libraryapi/services/BookServiceTest.java
@@ -1,9 +1,11 @@
 package br.com.libraryapi.services;
 
 import br.com.libraryapi.api.services.BookService;
+import br.com.libraryapi.exceptions.BusinessException;
 import br.com.libraryapi.model.entity.Book;
 import br.com.libraryapi.api.services.impl.BookServiceImpl;
 import br.com.libraryapi.model.repositories.BookRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -32,8 +34,9 @@ public class BookServiceTest {
     @Test
     @DisplayName("Deve salvar um livro")
     public void saveBookTest() {
-        Book book = Book.builder().isbn("123").author("Zé").title("Homem de Ferro").build();
+        Book book = createValidBook();
         Book mock = Book.builder().id(1L).isbn("123").author("Zé").title("Homem de Ferro").build();
+        Mockito.when( repository.existsByIsbn(Mockito.anyString()) ).thenReturn(false);
         Mockito.when( repository.save(book) ).thenReturn(mock);
 
         Book savedBook = service.save(book);
@@ -42,5 +45,24 @@ public class BookServiceTest {
         assertThat(savedBook.getIsbn()).isEqualTo("123");
         assertThat(savedBook.getTitle()).isEqualTo("Homem de Ferro");
         assertThat(savedBook.getAuthor()).isEqualTo("Zé");
+    }
+
+    @Test
+    @DisplayName("Deve lançar erro de negocio ao tentar salvar um livro com isbn duplicado")
+    public void shouldNotSaveBookWithDuplicatedISBN() {
+        Book book = createValidBook();
+        Mockito.when( repository.existsByIsbn(Mockito.anyString()) ).thenReturn(true);
+
+        Throwable exception = Assertions.catchThrowable( () -> service.save(book) );
+
+        assertThat(exception)
+                .isInstanceOf(BusinessException.class)
+                .hasMessage("Isbn já cadastrado.");
+        Mockito.verify(repository, Mockito.never()).save(book);
+    }
+
+
+    private Book createValidBook() {
+        return Book.builder().isbn("123").author("Zé").title("Homem de Ferro").build();
     }
 }
