5d2831f4c48ced2176fc48c060211c76d9977b57
Jean <jxs.jean@gmail.com>
2020-11-29 09:22:46 -0300

    Implements filter in BookService

--- src/main/java/br/com/libraryapi/api/services/BookService.java
+++ src/main/java/br/com/libraryapi/api/services/BookService.java
@@ -1,6 +1,8 @@
 package br.com.libraryapi.api.services;
 
 import br.com.libraryapi.model.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -11,4 +13,6 @@ public interface BookService {
     Optional<Book> getById(Long id);
     void delete(Book book);
     Book update(Book book);
+
+    Page<Book> find(Book filter, Pageable request);
 }
--- src/main/java/br/com/libraryapi/api/services/impl/BookServiceImpl.java
+++ src/main/java/br/com/libraryapi/api/services/impl/BookServiceImpl.java
@@ -4,6 +4,10 @@ import br.com.libraryapi.api.services.BookService;
 import br.com.libraryapi.exceptions.BusinessException;
 import br.com.libraryapi.model.entity.Book;
 import br.com.libraryapi.model.repositories.BookRepository;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -43,5 +47,15 @@ public class BookServiceImpl implements BookService {
             throw new IllegalArgumentException("Book id can't be null");
         }
         return repository.save(book);
+    }
+
+    @Override
+    public Page<Book> find(Book filter, Pageable request) {
+        Example<Book> example = Example.of(filter,
+                ExampleMatcher.matching()
+                        .withIgnoreCase()
+                        .withIgnoreNullValues()
+                        .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING));
+        return repository.findAll(example, request);
     }
 }
