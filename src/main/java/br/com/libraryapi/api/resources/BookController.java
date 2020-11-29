aadc5cacb43c56d6342405dd82b03fa08339ff40
Jean <jxs.jean@gmail.com>
2020-11-29 09:20:06 -0300

    Implements filter in BookController using pagination.

--- src/main/java/br/com/libraryapi/api/resources/BookController.java
+++ src/main/java/br/com/libraryapi/api/resources/BookController.java
@@ -6,6 +6,9 @@ import br.com.libraryapi.api.services.BookService;
 import br.com.libraryapi.exceptions.BusinessException;
 import br.com.libraryapi.model.entity.Book;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -13,7 +16,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/books")
@@ -60,6 +65,18 @@ public class BookController {
     public void delete(@PathVariable Long id) {
         Book book = bookService.getById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
         bookService.delete(book);
+    }
+
+    @GetMapping
+    public Page<BookDTO> find(BookDTO dto, Pageable pageable) {
+        Book filter = modelMapper.map(dto, Book.class);
+        Page<Book> result = bookService.find(filter, pageable);
+
+        List<BookDTO> books = result.getContent().stream()
+                .map(entity -> modelMapper.map(entity, BookDTO.class))
+                .collect(Collectors.toList()) ;
+
+        return new PageImpl<BookDTO>(books, pageable, books.size());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
