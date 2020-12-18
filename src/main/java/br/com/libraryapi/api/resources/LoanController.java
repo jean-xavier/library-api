db8197e4fe19851a78edf02de426248d6f8a2357
Jean <jxs.jean@gmail.com>
2020-12-17 21:58:38 -0300

    Implements endpoint for create new loan.

---
+++ src/main/java/br/com/libraryapi/api/resources/LoanController.java
@@ -0,0 +1,42 @@
+package br.com.libraryapi.api.resources;
+
+import br.com.libraryapi.api.dto.LoanDTO;
+import br.com.libraryapi.api.services.BookService;
+import br.com.libraryapi.api.services.LoanService;
+import br.com.libraryapi.model.entity.Book;
+import br.com.libraryapi.model.entity.Loan;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.LocalDate;
+
+@RestController
+@RequestMapping("/api/loans")
+public class LoanController {
+    private final BookService bookService;
+    private final LoanService loanService;
+
+    public LoanController(BookService bookService, LoanService loanService) {
+        this.bookService = bookService;
+        this.loanService = loanService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Long create(@RequestBody LoanDTO dto) {
+        Book book = bookService
+                .getBookByIsbn(dto.getIsbn())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Book not found for passed isbn"));
+
+        Loan loan = Loan.builder().
+                book(book)
+                .customer(dto.getCustomer())
+                .loanDate(LocalDate.now())
+                .build();
+
+        loan = loanService.save(loan);
+
+        return loan.getId();
+    }
+}
