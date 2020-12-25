de653be41c3bfa006bbc71b370575133b8232c43
Jean Xavier <jxs.jean@gmail.com>
2020-12-25 15:56:13 -0300

    Mapping entiry to JPA.

--- src/main/java/br/com/libraryapi/model/entity/Loan.java
+++ src/main/java/br/com/libraryapi/model/entity/Loan.java
@@ -5,16 +5,25 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Entity
 public class Loan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String customer;
+
+    @ManyToOne
     private Book book;
+
     private LocalDate loanDate;
+
     private Boolean returned;
 }
