be2205359a4e50fe124832a6caabbd355791eefd
Jean Xavier <jxs.jean@gmail.com>
2020-12-26 15:37:55 -0300

    Implements update endpoint in LoanController.

--- src/main/java/br/com/libraryapi/api/resources/LoanController.java
+++ src/main/java/br/com/libraryapi/api/resources/LoanController.java
@@ -1,6 +1,7 @@
 package br.com.libraryapi.api.resources;
 
 import br.com.libraryapi.api.dto.LoanDTO;
+import br.com.libraryapi.api.dto.ReturnedLoanDTO;
 import br.com.libraryapi.api.services.BookService;
 import br.com.libraryapi.api.services.LoanService;
 import br.com.libraryapi.model.entity.Book;
@@ -38,5 +39,13 @@ public class LoanController {
         loan = loanService.save(loan);
 
         return loan.getId();
+    }
+
+    @PatchMapping("{id}")
+    public void returnBook(@PathVariable Long id, @RequestBody ReturnedLoanDTO dto) {
+        Loan loan = loanService.getById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        loan.setReturned(dto.getReturned());
+
+        loanService.update(loan);
     }
 }
