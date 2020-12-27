daf780ac31434ea3832616d00a0a3bc554bd151c
Jean Xavier <jxs.jean@gmail.com>
2020-12-26 22:04:10 -0300

    Implements unit tests on custom method made in repository and used by Service layer.

--- src/test/java/br/com/libraryapi/services/LoanServiceTest.java
+++ src/test/java/br/com/libraryapi/services/LoanServiceTest.java
@@ -1,5 +1,6 @@
 package br.com.libraryapi.services;
 
+import br.com.libraryapi.api.dto.LoanFilterDTO;
 import br.com.libraryapi.api.services.LoanService;
 import br.com.libraryapi.api.services.impl.LoanServiceImpl;
 import br.com.libraryapi.exceptions.BusinessException;
@@ -12,10 +13,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.*;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -131,5 +135,36 @@ public class LoanServiceTest {
 
         assertThat(updatedLoan.getReturned()).isTrue();
         verify(repository).save(loan);
+    }
+
+    @Test
+    @DisplayName("Deve filtrar empréstimos pelas propriedades")
+    public void findLoanTest() {
+        final String customer = "João";
+        final String isbn = "12547";
+
+        LoanFilterDTO filterDTO = LoanFilterDTO.builder().customer(customer).isbn(isbn).build();
+
+        Book book = Book.builder().id(15L).isbn(isbn).title("Searching").author("Pedro").build();
+        Loan loan = Loan.builder().id(1L).loanDate(LocalDate.now()).customer(customer).book(book).build();
+
+        PageRequest pageRequest = PageRequest.of(0, 10);
+
+        List<Loan> loans = Collections.singletonList(loan);
+
+        Page<Loan> page = new PageImpl<Loan>(loans, pageRequest, loans.size());
+
+        Mockito.when(repository.findByBookIsbnOrCustomer(
+                    Mockito.anyString(),
+                    Mockito.anyString(),
+                    Mockito.any(Pageable.class)))
+                .thenReturn(page);
+
+        Page<Loan> result = service.find(filterDTO, pageRequest);
+
+        assertThat(result.getTotalElements()).isEqualTo(1);
+        assertThat(result.getContent()).isEqualTo(loans);
+        assertThat(result.getPageable().getPageNumber()).isEqualTo(0);
+        assertThat(result.getPageable().getPageSize()).isEqualTo(10);
     }
 }
