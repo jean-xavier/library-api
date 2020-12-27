0276b554dca924fbac619dd3ed063e9238043fb6
Jean Xavier <jxs.jean@gmail.com>
2020-12-26 21:37:38 -0300

    Add id in LoanDTO

--- src/main/java/br/com/libraryapi/api/dto/LoanDTO.java
+++ src/main/java/br/com/libraryapi/api/dto/LoanDTO.java
@@ -10,6 +10,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class LoanDTO {
+    private Long id;
     private String isbn;
     private String customer;
+    private BookDTO book;
 }
