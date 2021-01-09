8640d500016d8eaeaabb216d85f9fd65402e5ac0
Jean Xavier <jxs.jean@gmail.com>
2021-01-09 10:40:36 -0300

    Implements get loans by book.

--- src/main/java/br/com/libraryapi/api/services/LoanService.java
+++ src/main/java/br/com/libraryapi/api/services/LoanService.java
@@ -1,11 +1,13 @@
 package br.com.libraryapi.api.services;
 
 import br.com.libraryapi.api.dto.LoanFilterDTO;
+import br.com.libraryapi.model.entity.Book;
 import br.com.libraryapi.model.entity.Loan;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,4 +19,6 @@ public interface LoanService {
     Loan update(Loan loan);
 
     Page<Loan> find(LoanFilterDTO filter, Pageable page);
+
+    Page<Loan> getLoansByBook(Book book, Pageable pageable);
 }
--- src/main/java/br/com/libraryapi/api/services/impl/LoanServiceImpl.java
+++ src/main/java/br/com/libraryapi/api/services/impl/LoanServiceImpl.java
@@ -3,13 +3,18 @@ package br.com.libraryapi.api.services.impl;
 import br.com.libraryapi.api.dto.LoanFilterDTO;
 import br.com.libraryapi.api.services.LoanService;
 import br.com.libraryapi.exceptions.BusinessException;
+import br.com.libraryapi.model.entity.Book;
 import br.com.libraryapi.model.entity.Loan;
 import br.com.libraryapi.model.repositories.LoanRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
+@Service
 public class LoanServiceImpl implements LoanService {
     private final LoanRepository repository;
 
@@ -41,5 +46,10 @@ public class LoanServiceImpl implements LoanService {
     @Override
     public Page<Loan> find(LoanFilterDTO filter, Pageable pageable) {
         return repository.findByBookIsbnOrCustomer(filter.getIsbn(), filter.getCustomer(), pageable);
+    }
+
+    @Override
+    public Page<Loan> getLoansByBook(Book book, Pageable pageable) {
+        return repository.findByBook(book, pageable);
     }
 }
